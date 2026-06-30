59b7777584f9f72ac238b5f0892682ab9ffde6ea
Samson Tontoye <godfatherfigure1@gmail.com>
2026-06-30 12:51:37 +0100

    completed week 1 day 2 code exercises and homework

---
+++ month1/week1/labs/day2/day2.py
@@ -0,0 +1,57 @@
+name = "Samson"
+age = 25
+height = 1.82
+is_engineer = True
+
+
+print(type(age))
+print(type(name))
+print(type(height))
+print(type(is_engineer))
+
+name = input("What is your name? ")
+print("Hello", name)
+
+# f string
+name = input("What is your name? ")
+age = input("How old are you? ")
+country = input("What country are you from? ")
+print(f"Welcome {name}, you are {age} years old, and you live in {country}")
+
+name = "Samson"
+print(name)
+
+first_name = "Sony"
+last_name = "Ericsson"
+age = 50
+country = "Sweden"
+profession = "engineer"
+
+# exercise 1
+print(first_name)
+print(last_name)
+print(age)
+print(country)
+print(profession)
+
+# exercise 2
+print(type(first_name))
+print(type(last_name))
+print(type(age))
+print(type(country))
+print(type(profession))
+
+# exercise 3
+name = input("What is your name? ")
+age = int(input("How old are you? "))
+language = input("What is your favorite programming language? ")
+
+print(f"My name is {name}, I am {age} years old and my favorite programming language is {language}")
+
+# exercise 4
+name = "Samson"
+age = 23
+country = "Nigeria"
+profession = "Data Scientist/Machine Learning Engineer"
+
+print(f"Hello Everyone, my name is {name}, i am {age} years old. I am from {country} and i work as a {profession}")
---
+++ month1/week1/labs/day2/homework.md
@@ -0,0 +1,30 @@
+# Homework Day 2
+
+## Question 1
+What is a variable?
+
+Answer: A variable is a name that refers to an object stored in memory temporarily. You assign values to a variable. 
+
+
+## Question 2
+What is memory?
+
+Answer: A memory is a temporary location where data is stored.
+
+
+## Question 3
+Why is a variable not the same as a value?
+
+Answer: A value is the term that is being assigned to the variable while the variable is assigned to reference the object temporarily stored in memory.   
+
+
+## Question 4
+What does type() do?
+Answer: Type returns the type of the value
+
+
+## Question 5
+Why are meaningful variable names important?
+
+Answer: Meaningful variable names are important because it allows the code to be easily read by engineers.
+
